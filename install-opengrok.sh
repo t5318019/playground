3b42d7aee9fd219b185fab80832e04c87b77928a
Zheng-Yuan Wu <t5318019@hotmail.com>
2017-04-24 15:39:34 +0800

    更新如何安裝 OpenGrok

--- install-opengrok.sh
+++ install-opengrok.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 sudo apt-get -y install openjdk-8-jre tomcat8 exuberant-ctags
-sudo mkdir -p /var/opengrok/etc
+sudo mkdir -p /var/opengrok/etc /var/opengrok/src
 sudo chown -R tomcat8: /var/opengrok/
 wget https://github.com/OpenGrok/OpenGrok/releases/download/1.0/opengrok-1.0.tar.gz
 tar xzf opengrok-1.0.tar.gz
-sudo opengrok-1.0/bin/OpenGrok deploy
+sudo -u tomcat8 opengrok-1.0/bin/OpenGrok deploy
+sleep 30
+sudo -u tomcat8 opengrok-1.0/bin/OpenGrok index
