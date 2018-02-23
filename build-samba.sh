c3aacb0a3c159d8fe2f627812c458b947adbd917
Zheng-Yuan Wu <t5318019@hotmail.com>
2018-02-23 12:15:00 +0800

    安裝 PAM 程式庫讓 configure 成功

--- build-samba.sh
+++ build-samba.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git clone https://github.com/samba-team/samba.git
 cd samba/
-sudo apt-get -y install python-dev libgnutls-dev libacl1-dev libldap2-dev
+sudo apt-get -y install python-dev libgnutls-dev libacl1-dev libldap2-dev libpam0g-dev
 ./configure
 make
