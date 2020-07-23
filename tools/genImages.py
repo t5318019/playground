a9f0b24385d4f7c1ce8822bdfe89607c738fe7f2
Zheng-Yuan Wu <t5318019@hotmail.com>
2020-07-23 17:25:46 +0800

    增加產生 JPEG 格式

--- tools/genImages.py
+++ tools/genImages.py
@@ -4,20 +4,26 @@ try:
     from PIL import Image
     from PIL import ImageColor
 except ModuleNotFoundError:
-    exit("請安裝 Python Imaging Library, 指令: pip install Pillow")
+    exit("請安裝 Python Imaging Library 7.2.0, 指令: pip install Pillow")
 
 
 def main():
-    """ 產生 148 個不同顏色的 PNG 圖片檔案 """
+    """ 產生 148 個不同顏色的 PNG, JPEG 圖片檔案 """
     current_dir = (os.path.abspath(os.path.dirname(__file__)))
-    output_dir = current_dir + os.sep + "output"
-    os.path.exists(output_dir) or os.mkdir(output_dir)
+    output_png_dir = current_dir + os.sep + "output" + os.sep + "png"
+    output_jpg_dir = current_dir + os.sep + "output" + os.sep + "jpg"
+    os.makedirs(output_png_dir, exist_ok=True)
+    os.makedirs(output_jpg_dir, exist_ok=True)
     for color_name in ImageColor.colormap:
-        filename = output_dir + os.sep + color_name + ".png"
-        if os.path.exists(filename):
-            continue
         im = Image.new("RGB", (800, 600), color_name)
-        im.save(filename)
+
+        png_filename = output_png_dir + os.sep + color_name + ".png"
+        if not os.path.exists(png_filename):
+            im.save(png_filename)
+
+        jpg_filename = output_jpg_dir + os.sep + color_name + ".jpg"
+        if not os.path.exists(jpg_filename):
+            im.save(jpg_filename)
 
 
 if __name__ == "__main__":
