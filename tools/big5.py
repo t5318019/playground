093a1f0a6aa682c2e4383a4dca5733b6245b4151
Zheng-Yuan Wu <t5318019@hotmail.com>
2020-08-14 17:28:47 +0800

    新增 Big5 字碼表

---
+++ tools/big5.py
@@ -0,0 +1,40 @@
+import os
+
+
+def main():
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    md_file = os.path.join(this_dir, '../docs/big5.md')
+    md_file = os.path.normpath(md_file)
+
+    f = open(md_file, 'w', encoding="utf-8")
+    print('# Big5\n', file=f)
+    print('## 常用漢字\n', file=f)
+
+    header = ['\u3000', ]
+    for i in range(16):
+        header.append(i.to_bytes(1, 'big').hex()[1])
+    print('|', '| '.join(header), '|', sep='', file=f)
+    print('|--'*17, '| ', sep='', file=f)
+
+    # 常用漢字 (0xA440-0xC67E)
+    c = int.from_bytes(bytes.fromhex('a440'), 'big')
+    s = []
+    while c <= int.from_bytes(bytes.fromhex('c67e'), 'big'):
+        try:
+            character = c.to_bytes(2, 'big').decode('big5')
+        except:
+            character = '\u3000'
+        s.append(character)
+        if (len(s) == 16):
+            start = (c-15).to_bytes(2, 'big').hex()
+            print('|', start, '|', '|'.join(s), '|', sep='', file=f)
+            s = []
+        c = c + 1
+    start = (c-15).to_bytes(2, 'big').hex()
+    print('|', start, '|', '|'.join(s), '|', sep='', file=f)
+
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
