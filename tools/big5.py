c6b116b1b18f12a1261254c34f94b2056208916b
Zheng-Yuan Wu <t5318019@hotmail.com>
2020-08-14 17:51:54 +0800

    修正 Markdown 表格問題

--- tools/big5.py
+++ tools/big5.py
@@ -3,7 +3,7 @@ import os
 
 def main():
     this_dir = os.path.dirname(os.path.abspath(__file__))
-    md_file = os.path.join(this_dir, '../docs/big5.md')
+    md_file = os.path.join(this_dir, '../docs/Big5.md')
     md_file = os.path.normpath(md_file)
 
     f = open(md_file, 'w', encoding="utf-8")
@@ -31,6 +31,9 @@ def main():
             s = []
         c = c + 1
     start = (c-15).to_bytes(2, 'big').hex()
+    if(len(s) < 16):
+        padding = ['\u3000']*(16-len(s))
+        s.extend(padding)
     print('|', start, '|', '|'.join(s), '|', sep='', file=f)
 
     f.close()
