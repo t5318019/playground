ed3d0c8c9c8518ca9f14f1d20d9c4bb946666a90
Zheng-Yuan Wu <t5318019@hotmail.com>
2021-01-14 18:07:49 +0800

    新增產生 GIF 圖片檔案

--- tools/genImages.py
+++ tools/genImages.py
@@ -8,22 +8,19 @@ except ModuleNotFoundError:
 
 
 def main():
-    """ 產生 148 個不同顏色的 PNG, JPEG 圖片檔案 """
+    """ 產生 148 個不同顏色的 PNG, JPEG, GIF 圖片檔案 """
     current_dir = (os.path.abspath(os.path.dirname(__file__)))
-    output_png_dir = current_dir + os.sep + "output" + os.sep + "png"
-    output_jpg_dir = current_dir + os.sep + "output" + os.sep + "jpg"
-    os.makedirs(output_png_dir, exist_ok=True)
-    os.makedirs(output_jpg_dir, exist_ok=True)
-    for color_name in ImageColor.colormap:
-        im = Image.new("RGB", (800, 600), color_name)
+    image_types = ["png", "jpg", "gif"]
 
-        png_filename = output_png_dir + os.sep + color_name + ".png"
-        if not os.path.exists(png_filename):
-            im.save(png_filename)
+    for image_type in image_types:
+        output_dir = current_dir + os.sep + "output" + os.sep + image_type
+        os.makedirs(output_dir, exist_ok=True)
 
-        jpg_filename = output_jpg_dir + os.sep + color_name + ".jpg"
-        if not os.path.exists(jpg_filename):
-            im.save(jpg_filename)
+        for color_name in ImageColor.colormap:
+            im = Image.new("RGB", (800, 600), color_name)
+            filename = output_dir + os.sep + color_name + "." + image_type
+            if not os.path.exists(filename):
+                im.save(filename)
 
 
 if __name__ == "__main__":
