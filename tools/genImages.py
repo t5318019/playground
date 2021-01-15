79c3aa1e5c418258369ebd32f58ae4f728658f68
Zheng-Yuan Wu <t5318019@hotmail.com>
2021-01-15 09:11:50 +0800

    產生不同大小的 PNG, JPEG, GIF 圖片

--- tools/genImages.py
+++ tools/genImages.py
@@ -8,25 +8,11 @@ except ModuleNotFoundError:
 
 
 def main():
-    """ 產生 148 個不同顏色的 PNG, JPEG, GIF 圖片檔案 """
+
     current_dir = (os.path.abspath(os.path.dirname(__file__)))
     image_types = ("png", "jpg", "gif")
-
-    for image_type in image_types:
-        output_dir = current_dir + os.sep + "output" + os.sep + image_type
-        os.makedirs(output_dir, exist_ok=True)
-
-        for color_name in ImageColor.colormap:
-            im = Image.new("RGB", (800, 600), color_name)
-            filename = output_dir + os.sep + color_name + "." + image_type
-            if not os.path.exists(filename):
-                im.save(filename)
-
-    """ 產生不同大小的 JPEG 圖片 """
-    output_dir = current_dir + os.sep + "output" + os.sep + "size"
-    os.makedirs(output_dir, exist_ok=True)
-
-    sizes = (
+    default_image_size = (800, 600)
+    image_sizes = (
         (320, 240),
         (640, 480),
         (800, 600),
@@ -39,13 +25,33 @@ def main():
         (3840, 2460),
         (7680, 4320),
         (15360, 8640),
+        (1, 1),
+        (10, 10),
+        (100, 100),
+        (1000, 1000),
+        (10000, 10000),
+        (1, 65535),
+        (65535, 1),
     )
 
-    for size in sizes:
-        im = Image.new("RGB", size, "cyan")
-        filename = output_dir + os.sep + "{}x{}.jpg".format(*size)
-        if not os.path.exists(filename):
-            im.save(filename)
+    """ 產生 PNG, JPEG, GIF 圖片檔案 """
+    for image_type in image_types:
+        output_dir = current_dir + os.sep + "output" + os.sep + image_type
+        os.makedirs(output_dir, exist_ok=True)
+
+        """ 產生 148 個不同顏色的圖片 """
+        for color_name in ImageColor.colormap:
+            im = Image.new("RGB", default_image_size, color_name)
+            filename = output_dir + os.sep + color_name + "." + image_type
+            if not os.path.exists(filename):
+                im.save(filename)
+
+        """ 產生不同大小的 JPEG 圖片 """
+        for size in image_sizes:
+            im = Image.new("RGB", size, "cyan")
+            filename = output_dir + os.sep + "{}x{}.png".format(*size)
+            if not os.path.exists(filename):
+                im.save(filename)
 
 
 if __name__ == "__main__":
